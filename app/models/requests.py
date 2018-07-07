5505015cfd08c3c8e867bac572a0eb2cd31313f5
nakimera <prossienakimera@gmail.com>
2018-07-07 23:56:39 +0300

    create class request

---
+++ app/models/requests.py
@@ -0,0 +1,6 @@
+class Request():
+    def __init__(self, category, item_name, quantity, description):
+        self.category = category
+        self.item_name = item_name
+        self.quantity = quantity
+        self.description = description
