0f578e348ec09a329f02340a4823fdbed525ca4b
nakimera <prossienakimera@gmail.com>
2018-07-16 17:05:19 +0300

    create and register auth blueprint

---
+++ app/api/v1/auth/views.py
@@ -0,0 +1,56 @@
+users_list = []
+
+@app.route('/api/v1/users/login', methods=['POST'])
+def login_user(): 
+    if request.method == 'POST':
+        data = request.get_json(force = True)
+        username = data.get("username", None)
+        password = data.get("password", None)
+
+        for user in users_list:
+
+            if user.username == username and user.password == password: 
+                return jsonify({
+                    "message": "{} logged in".format(username),
+                    "status": True
+                }), 200
+            
+        return jsonify({
+                    "message": "Either username or password is incorrect",
+                    "status": False
+                }), 404
+
+@app.route('/api/v1/users', methods=['POST', 'GET'])
+def users():
+    if request.method == 'POST':
+        data = request.get_json(force=True)
+        username = data.get("username", None)
+        password = data.get("password", None)
+
+        if not username:
+            return jsonify({'message': 'Please provide a username'}), 400  
+
+        elif not password:
+            return jsonify({'message': 'Please provide a password'}), 400    
+
+        else:
+            user = User(username, password)
+            users_list.append(user)
+            return jsonify({
+                "message": "User sucessfully created",
+                "status": True,
+                "data": {
+                    "id": user.id,
+                    "username": "{}".format(username)
+                    }
+                }), 201
+
+    if request.method == 'GET':
+        usernames = []
+
+        for user in users_list:
+            usernames.append(user.username)
+        return jsonify({
+            "message": "Users successfully retrieved",
+            "status": True, 
+            "data": "{}".f
